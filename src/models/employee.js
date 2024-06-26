51c5096674e6ca08c3102d34dbe4cf2e18e42845
omarques <netoguitars@gmail.com>
2024-06-26 18:06:04 -0300

    estrutura de arquivos e modelo de funcionário

---
+++ src/models/employee.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(id, name, age, role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+}
+
+module.exports = Employee;
