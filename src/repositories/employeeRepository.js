6b9464e480009ba2384ee8ad36dfc6c1ea7ecef0
omarques <netoguitars@gmail.com>
2024-06-26 18:11:23 -0300

    repositorio de integração com o banco de dados

---
+++ src/repositories/employeeRepository.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.DYNAMODB_TABLE;
+
+class EmployeeRepository {
+    async createEmployee(employee) {
+        const params = {
+            TableName: tableName,
+            Item: employee
+        };
+        await dynamoDb.put(params).promise();
+        return employee;
+    }
+
+    async getEmployeeById(id) {
+        const params = {
+            TableName: tableName,
+            Key: { id }
+        };
+        const result = await dynamoDb.get(params).promise();
+        return result.Item;
+    }
+
+    async updateEmployee(employee) {
+        const params = {
+            TableName: tableName,
+            Item: employee
+        };
+        await dynamoDb.put(params).promise();
+        return employee;
+    }
+
+    async deleteEmployee(id) {
+        const params = {
+            TableName: tableName,
+            Key: { id }
+        };
+        await dynamoDb.delete(params).promise();
+    }
+}
+
+module.exports = EmployeeRepository;
